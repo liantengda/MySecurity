1ed255616ee941de76bbb1df16646a7ad8a1eab1
liantengda <1666893831@qq.com>
2020-08-24 10:17:31 +0800

    啥也不是

--- src/main/java/com/lian/mysecurity/MySecurityApplication.java
+++ src/main/java/com/lian/mysecurity/MySecurityApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class MySecurityApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         SpringApplication.run(MySecurityApplication.class, args);
     }
 
